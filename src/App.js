3ada57e037a8034db0bf5b53d18e2a6e55d6328d
Gautam-XIII <gautamjinjala999@gmail.com>
2024-03-19 17:23:20 +0530

    Update for counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import {useRef} from "react";
+
 function App() {
 
-  let Number = 0;
+  // use for simlpe javascript
+  // let Number = 0;
+
+  // const addValue = () => {
+  //   Number = Number + 1;
+  //   document.getElementById("output").innerHTML = Number;
+  // };
+
+  // const removeValue = () => {
+  //   Number = Number - 1;
+  //   document.getElementById("output").innerHTML = Number;
+  // };
+
+
+  // Using React useRef()
+  const counter = useRef();
 
   const addValue = () => {
-    Number = Number + 1;
-    document.getElementById("output").innerHTML =  Number;
+    const last = parseInt(counter.current.innerHTML);
+    counter.current.innerHTML = last + 1;
   }
 
   const removeValue = () => {
-    Number = Number - 1;
-    document.getElementById("output").innerHTML =  Number;  
+    const last = parseInt(counter.current.innerHTML);
+    counter.current.innerHTML = last - 1;
   }
 
   return (
     <>
-      <h2 id="output"> {Number} </h2>
-      <button onClick={addValue}>Add</button>
-      <button onClick={removeValue}>Remove</button>
+      {/* <div>
+        <h2 id="output"> {Number} </h2>
+        <button onClick={addValue}>Add</button>
+        <button onClick={removeValue}>Remove</button>
+      </div> */}
+
+      <div>
+      <h2 ref={counter}> 0 </h2>
+      <button  onClick={() => addValue()}>Add</button>
+      <button  onClick={() => removeValue()}>Remove</button>
+      </div>
     </>
   );
 }
